236a5a3a086363d9d686935b89f796ba8455225c
Ryaaad <87044522+Ryaaad@users.noreply.github.com>
2023-03-25 22:23:31 +0100

    Last Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import './App.css';
+import decor from "./Images/MenuDecoration.png"
 import Backround from './Images/Backround.png'
-import img from './Images/plat.png'
-import Menu from "./Componants/Menu"
-import Item from "./Componants/Items"
+import img from './Images/salad.png'
+
 function App() {
-  const A=[1,2,3,4]
   return (
     <div style={{backgroundImage: `url(${Backround})` }} className="App ">
     <img src={img} alt="" className='ImgCenter' />
-    <Menu></Menu>
-    <div className="Itemcontainer">
-    {A.map(a=>{
-      return <Item></Item>
-    })}
+   
+    <div  className="Menu" >
+            <div className="container_Menu">
+         <h2 className="Menu_Title">Menu</h2>
+    <img src={decor} alt="" className="Menu_Img" />
+            </div>
+      <div className="Types_Container">
+    
+      </div>
+
+        </div>
+
+    <div className="Item_container">
+   
     </div>
   
     </div>
