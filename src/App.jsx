16e5df2e46a07a7f3ece3d4b1eb7810ba6c04e0a
Ryaaad <87044522+Ryaaad@users.noreply.github.com>
2023-03-26 01:58:39 +0100

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,29 @@ import Menu from './Componants/Menu'
 import Item from './Componants/Item'
 function App() {
   const [Data, setData] = useState([]);
+  const [Type,setType]=useState("Chicken");
   useEffect(() => {
     async function fetchData() {
-      const response =await fetch(`${process.env.PUBLIC_URL}/data/ChickenPlates.json`);
+      let Url;
+      if(Type==="Chicken") { Url=`${process.env.PUBLIC_URL}/data/ChickenPlates.json` }
+      if(Type==="Salad") { Url=`${process.env.PUBLIC_URL}/data/SaladPlates.json` }
+      if(Type==="Sea") { Url=`${process.env.PUBLIC_URL}/data/SeaFoodPlates.json` }
+      const response =await fetch(Url);
       const json = await response.json();
       console.log(json);
-      console.log(response);
       setData(json);
     }
 
     fetchData();
 
-  }, []);
+  }, [Type]);
   return (
     <div style={{backgroundImage: `url(${Backround})` }} className="App ">
     <img src={img} alt="" className='ImgCenter' />
-    <Menu></Menu>
+    <Menu setType={setType} ></Menu>
     <div className="Item_container">
    {Data.map(data=>{
-    return <Item Name={data.Name} Calories={data.Calories} img={data.img} Time={data.Time} person={data.person} ></Item>
+    return <Item key={data.Name}  Name={data.Name} Calories={data.Calories} img={data.img} Time={data.Time} person={data.person} ></Item>
    })}
     </div>
   
--- src/Componants/Menu.jsx
+++ src/Componants/Menu.jsx
@@ -1,6 +1,6 @@
 import decor from "../Images/MenuDecoration.png"
 import { useEffect,useState } from "react"
-const Menu = () => {
+const Menu = (props) => {
     const [Data, setData] = useState([]);
 
     useEffect(() => {
@@ -8,7 +8,6 @@ const Menu = () => {
         const response =await fetch(`${process.env.PUBLIC_URL}/data/Type.json`);
         const json = await response.json();
         console.log(json);
-        console.log(response);
         setData(json);
       }
 
@@ -23,7 +22,7 @@ const Menu = () => {
             </div>
       <div className="Types_Container">
       {Data.map(data=>{
-        return <div key={data.Type} className="Type" >
+        return <div key={data.Type} className="Type" onClick={()=> props.setType(data.Type) } >
                <div className="Type_Gap">
                <img src={data.Type_Img} alt="" className="Type_img" />
                <p className="Type_Text">{data.Type}</p>
